4cdc1bbfa416b72f77a4ac4428278132163243fc
ElliotBeckett <elliotbeckett1@gmail.com>
2021-06-29 14:40:45 +1000

    Initial Commit

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private static List<Character> characters = new List<Character>{
+            new Character(), 
+            new Character { ID = 1, Name = "Sam"}
+        };
+        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
+        {
+            var serviceResponse = new ServiceResponse<List<Character>>();
+            characters.Add(newCharacter);
+            serviceResponse.Data = characters;
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
+        {
+            var serviceResponse = new ServiceResponse<List<Character>>();
+            serviceResponse.Data = characters;
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<Character>> GetCharacterByID(int id)
+        {
+            var serviceResponse = new ServiceResponse<Character>();
+            serviceResponse.Data = characters.FirstOrDefault(c => c.ID == id);
+            return serviceResponse;
+        }
+    }
+}
